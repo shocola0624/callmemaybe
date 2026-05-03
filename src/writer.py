61b4d34bd242df86ce0f4269508a7a43f47a0385
Shotaro Kotera <sho_kotera@yahoo.co.jp>
2026-05-03 18:26:39 +0900

    writer

--- src/writer.py
+++ src/writer.py
@@ -1,10 +1,11 @@
 from typing import List
 from .models import Output
+import json
 
 
 def write_output_file(path: str, data: List[Output]) -> None:
     """TODO"""
-    result = "[\n"
+    result = json.dump(data)
 
     try:
         with open(path, "w") as f:
