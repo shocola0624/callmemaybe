8b9655af06618fb8951bcb184c65df1b6e906708
Shotaro Kotera <sho_kotera@yahoo.co.jp>
2026-05-04 12:35:34 +0900

    color definition

--- src/defines.py
+++ src/defines.py
@@ -5,3 +5,19 @@ OUTPUT_PATH = "data/output/function_calls.json"
 
 # Generate Function Calling
 MAX_TOKENS_FOR_EACH_CALL = 64  # for function name or parameter values
+
+# Colors
+RESET = "\x1b[0m"
+RED = "\x1b[38;5;9m"
+DARKRED = "\x1b[38;5;1m"
+ORANGE = "\x1b[38;5;214m"
+YELLOW = "\x1b[38;5;11m"
+LIME = "\x1b[38;5;10m"
+GREEN = "\x1b[38;5;2m"
+CYAN = "\x1b[38;5;4m"
+BLUE = "\x1b[38;5;21m"
+MAGENTA = "\x1b[38;5;198m"
+PURPLE = "\x1b[38;5;93m"
+MAROON = "\x1b[38;5;124m"
+BROWN = "\x1b[38;5;94m"
+BLACK = "\x1b[38;5;16m"
