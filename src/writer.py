efd9bbcc18d0e71bf52b3f16ba20938435a18274
Shotaro Kotera <sho_kotera@yahoo.co.jp>
2026-05-04 22:04:08 +0900

    mkdir

--- src/writer.py
+++ src/writer.py
@@ -1,15 +1,28 @@
 from typing import List
 from .models import Output
+from pathlib import Path
 import json
 
 
-def write_output_file(path: str, data: List[Output]) -> None:
-    """TODO"""
-    try:
-        with open(path, "w") as f:
-            json.dump(data, f, indent=4)
+def write_output_file(file_path: str, data: List[Output]) -> None:
+    """Write results to the output JSON file."""
+    output_file = Path(file_path)
 
-    except OSError:
+    # Create directory
+    try:
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+
+    except OSError as e:
         raise ValueError(
-            f"Error ({path}): An error occurred while writing {path}."
+            f"Error: Failed to create output directory '{file_path}': {e}"
+        )
+
+    # Create output file and write result
+    try:
+        with output_file.open("w") as f:
+            json.dump([d.model_dump() for d in data], f, indent=4)
+
+    except OSError as e:
+        raise ValueError(
+            f"Error: Failed to write output file '{file_path}': {e}"
         )
